df4bbeceab595c897e4a72dc234bf7ea22c411d4
Sergey Mukhin <s.mukhin@cubicmedia.ru>
2025-10-24 21:49:52 +0300

    циферки

--- SkillF3/Program.cs
+++ SkillF3/Program.cs
@@ -4,9 +4,8 @@ class MainClass
 {
     static void Main(string[] args)
     {
-        Console.WriteLine(true);
-        Console.WriteLine(false);
-
-        Console.ReadKey();
+        Console.WriteLine(0x0A);
+        Console.WriteLine(0b11);
+        Console.WriteLine(5.5);
     }
 }
