a0b14d0a000a825f15bb2a5e5686ed201a1c19ed
Sergey Mukhin <s.mukhin@cubicmedia.ru>
2025-10-24 23:04:00 +0300

    TryParse

--- SkillF3/Program.cs
+++ SkillF3/Program.cs
@@ -5,7 +5,8 @@ class MainClass
     static void Main(string[] args)
     {
         Console.Write("Enter your age: ");
-        int age = int.Parse(Console.ReadLine());
+        int age;
+        bool iscorrect = int.TryParse(Console.ReadLine(), out age);
         Console.WriteLine("Your age is {0} ", age);
         Console.ReadKey();
     }
