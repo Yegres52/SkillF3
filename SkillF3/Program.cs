afe9cf49c63ead709b356be67be9cd9dabd81290
Sergey Mukhin <s.mukhin@cubicmedia.ru>
2025-10-24 21:46:58 +0300

    символьные литералы

--- SkillF3/Program.cs
+++ SkillF3/Program.cs
@@ -4,6 +4,16 @@ class MainClass
 {
     public static void Main(string[] args)
     {
-        Console.WriteLine("Привет,\n мир");
+
+        const string MyName = "Jane";
+
+        Console.WriteLine(MyName);
+
+        Console.WriteLine("\t Привет Мир");
+        Console.WriteLine("\t Мне 27 лет");
+        Console.WriteLine("\t My name is \n {0}", MyName);
+        Console.WriteLine("\u0023");
+        Console.ReadKey();
+
     }
 }
