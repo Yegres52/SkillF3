c2b4976e6a9f80761e2c2ab29499f81c5c173178
Sergey Mukhin <s.mukhin@cubicmedia.ru>
2025-10-24 22:17:53 +0300

    Перечисления

--- SkillF3/Program.cs
+++ SkillF3/Program.cs
@@ -2,12 +2,20 @@
 
 class MainClass
 {
-    static void Main(string[] args)
+    public static void Main(string[] args)
     {
-        const string name = "Sergey";
-        byte age = 22;
-        bool pet = true;
-        float size = 45;
-        Console.WriteLine("My name {0}\nMy age is {1}\nDo I have a pet? {2}, \nMy shoe size is {3}", name, age, pet, size);
+        DaysOfWeek MyFavoriteDay;
+
+        MyFavoriteDay = DaysOfWeek.Friday;
+
+        Console.WriteLine(MyFavoriteDay);
     }
+}
+
+enum DaysOfWeek : byte
+{
+    Tuesday,
+    Monday,
+    Wednesday,
+    Friday
 }
