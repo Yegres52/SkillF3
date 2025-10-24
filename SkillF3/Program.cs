77771ebe17c1e9eef490978f5f04e9809bc3f3c4
Sergey Mukhin <s.mukhin@cubicmedia.ru>
2025-10-24 22:44:55 +0300

    Матиматика урааа

--- SkillF3/Program.cs
+++ SkillF3/Program.cs
@@ -4,13 +4,7 @@ class MainClass
 {
     public static void Main(string[] args)
     {
-        string MyName = "Computer";
-        Console.WriteLine("Привет, человек");
-        Console.WriteLine("Меня зовут {0}", MyName);
-        Console.Write("А как тебя зовут? ");
-        string YourName = Console.ReadLine();
-        Console.Write("Рад встрече, {0} ", YourName);
-
-        Console.ReadKey();
+        double result = 5 / 2;
+        Console.WriteLine("5 / 2 = {0}", result);
     }
 }
